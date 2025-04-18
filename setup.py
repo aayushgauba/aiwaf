34e747b09a3c0ea7a693ade52e45dd4dbdb897be
Aayush Gauba <55630226+aayushgauba@users.noreply.github.com>
2025-04-17 22:38:14 -0500

    Bug Fixes

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="aiwaf",
     version="0.1.0",
     description="AI‑driven pluggable Web Application Firewall for Django (CSV or DB storage)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # <- required for markdown support
     author="Aayush Gauba",
     packages=find_packages(),
     package_data={
