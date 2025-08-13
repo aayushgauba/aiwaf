a3809b5801f8a13bb8cb73fdc52a3d1d4c0d5faa
Aayush Gauba <55630226+aayushgauba@users.noreply.github.com>
2025-08-12 21:49:24 -0500

    Bug Fixes

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-# setup.py    version="0.1.9.0.4",from setuptools import setup, find_packages
+# setup.py
+from setuptools import setup, find_packages
 import pathlib
 
 HERE = pathlib.Path(__file__).parent
@@ -8,7 +9,7 @@ long_description = (HERE / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="aiwaf",
-    version="0.1.9.0.3",
+    version="0.1.9.0.4",
     description="AI‑driven, self‑learning Web Application Firewall for Django",
     long_description=long_description,
     long_description_content_type="text/markdown",
